72999488c5f195427ccad12df5d0919e47a49e36
Trish <trishle729@gmail.com>
2025-04-12 13:36:03 -0400

    new comments

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,10 @@
 import unittest
 from calculator import *
 
+# https://github.com/Madam-TLE/lab10-TL-DS.git
+# Partner 1: Trish Le
+# Partner 2: Diego Salazar
+
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
@@ -11,12 +15,7 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
     def test_multiply(self):
         self.assertEqual(mul(2, 3), 6)
         self.assertEqual(mul(4, 5), 20)
@@ -43,30 +42,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
             logarithm(0, 5)
         self.assertEqual(logarithm(1, 5), 0)
         self.assertEqual(logarithm(1, 5), 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
     def test_hypotenuse(self):
         self.assertEqual(hypotenuse(2), 2)
         self.assertEqual(hypotenuse(3), 2)
         self.assertEqual(hypotenuse(4), 2)
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+
     def test_sqrt(self):
         with self.assertRaises(ValueError):
             square_root(0)
